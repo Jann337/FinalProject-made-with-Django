3cb65335efdaac8b1e6ddd66c25d9cf21520c259
Jann337 <145828833+Jann337@users.noreply.github.com>
2024-01-15 00:43:48 +0200

    deleted old comments

--- back_office/settings/views.py
+++ back_office/settings/views.py
@@ -3,31 +3,14 @@ from .models import Variables
 from django.http import HttpResponseBadRequest
 
 
-# def settings(request):
-#     variables = Variables.objects.first()
-
-#     if request.method == 'POST':
-#         website_title = request.POST.get("title")
-#         description = request.POST.get("description")
-#         currency = request.POST.get("currency")
-#         if website_title:
-#             variables.website_title = website_title
-#         if description:
-#             variables.homepage_description = description
-#         if currency:
-#             variables.website_currency = currency
-#     variables.save()
-#     return render(request, "settings.html", {'variables': variables})
-
-
 def settings(request):
     variables, created = Variables.objects.get_or_create(
-        pk=1)  # Assumes Variables has a primary key of 1
+        pk=1)
 
     if created:
-        # Initialize the newly created Variables object with default values
+
         variables.website_title = "Default Title"
-        # Set other default values
+
         variables.save()
 
     if request.method == 'POST':
